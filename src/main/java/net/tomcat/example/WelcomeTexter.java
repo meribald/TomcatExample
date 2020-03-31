6104ef21133a8e37df9cd230a49b0f11c29aee06
Erdinc Ciftci <erdinc.ciftci@comeon.com>
2020-03-31 11:27:22 +0300

    Fixes Guice-related injection error

--- src/main/java/net/tomcat/example/WelcomeTexter.java
+++ src/main/java/net/tomcat/example/WelcomeTexter.java
@@ -1,11 +1,6 @@
 package net.tomcat.example;
 
-import javax.inject.Named;
 
-import com.google.inject.servlet.RequestScoped;
-
-@Named
-@RequestScoped
 public class WelcomeTexter {
 
 	public String getGreeting() {
